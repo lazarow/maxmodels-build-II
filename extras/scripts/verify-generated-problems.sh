3323ce56332daa2a2a7ee72518c7226c4b132ebb
Arkadiusz Nowakowski <nowakowski.arkadiusz@gmail.com>
2026-08-13 14:38:32 +0200

    chore: add script to verify generated problems against expected output

---
+++ extras/scripts/verify-generated-problems.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+expected='1 1 0 0
+0
+0
+B+
+0
+B-
+1
+0
+1'
+
+find . -type f -name '*.lp' -print0 |
+while IFS= read -r -d '' file; do
+    result=$(your_command "$file")
+
+    if [ "$result" = "$expected" ]; then
+        printf '%s\n' "$file"
+    fi
+done
